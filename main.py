ff26793e9d6116161b09f5f636018e22299c8d7b
ScottBright <brightnegima@gmail.com>
2014-12-16 14:51:32 +0700

    First commit

--- main.py
+++ main.py
@@ -16,9 +16,8 @@ class SpaceGame(gamelib.SimpleGame):
         self.select = 1
         self.level = 1
         self.button = 0
-        self.score = 1000
-        self.totalscore = 0
         self.time = 45
+        self.mtime = 0
         self.ball = Ball(radius=10,
                          color=SpaceGame.WHITE,
                          pos=(200,
@@ -30,40 +29,48 @@ class SpaceGame(gamelib.SimpleGame):
     def init(self):
         super(SpaceGame, self).init()
         self.render_level()
+        self.render_level2()
 
     def update(self):
-        if self.is_key_pressed(K_RIGHT):
-            self.button+=1
-            if(self.select<3 and self.button == 1):
-                self.select+=1
-                self.ball.change(self.select)
-        elif self.is_key_pressed(K_LEFT):
-            self.button+=1
-            if(self.select>1 and self.button == 1):
-                self.select-=1
-                self.ball.change(self.select)
-        elif self.is_key_pressed(K_SPACE):
-            self.button += 1
-            if self.select == 1 and self.button == 1:
-                self.bar1.spacebar()
-            if self.select == 2 and self.button == 1:
-                self.bar2.spacebar()
-            if self.select == 3 and self.button == 1:
-                self.bar3.spacebar()
-        elif self.is_key_pressed(K_g):
-            if self.select == 1:
-                self.bar1.spacebar()
-            if self.select == 2:
-                self.bar2.spacebar()
-            if self.select == 3:
-                self.bar3.spacebar()
-        else:
-            self.button = 0
-        self.render_level()
-        self.bar1.update()
-        self.bar2.update()
-        self.bar3.update()
-        self.check_level()
+        if(self.time>0):
+            if self.is_key_pressed(K_RIGHT):
+                self.button+=1
+                if(self.select<3 and self.button == 1):
+                    self.select+=1
+                    self.ball.change(self.select)
+            elif self.is_key_pressed(K_LEFT):
+                self.button+=1
+                if(self.select>1 and self.button == 1):
+                    self.select-=1
+                    self.ball.change(self.select)
+            elif self.is_key_pressed(K_SPACE):
+                self.button += 1
+                if self.select == 1 and self.button == 1:
+                    self.bar1.spacebar()
+                if self.select == 2 and self.button == 1:
+                    self.bar2.spacebar()
+                if self.select == 3 and self.button == 1:
+                    self.bar3.spacebar()
+            elif self.is_key_pressed(K_g):
+                if self.select == 1:
+                    self.bar1.spacebar()
+                if self.select == 2:
+                    self.bar2.spacebar()
+                if self.select == 3:
+                    self.bar3.spacebar()
+            else:
+                self.button = 0
+            self.render_level()
+            self.render_level2()
+            self.bar1.update()
+            self.bar2.update()
+            self.bar3.update()
+            self.check_level()
+            self.mtime += 1
+            print self.time
+            if self.mtime > 60:
+                self.time -= 1
+                self.mtime = 0
           
     def check_level(self):
         if self.bar1.width > 320 and self.bar2.width > 320 and self.bar3.width > 320 :
@@ -84,12 +91,16 @@ class SpaceGame(gamelib.SimpleGame):
     def render_level(self):
         self.level_image = self.font.render("Level = %d" % self.level, 0, SpaceGame.WHITE)
 
+    def render_level2(self):
+        self.level_image2 = self.font.render("Time = %d" % self.time, 0, SpaceGame.WHITE)
+
     def render(self, surface):
         self.ball.render(surface)
         self.bar1.render(surface)
         self.bar2.render(surface)
         self.bar3.render(surface)
         surface.blit(self.level_image, (15,15))
+        surface.blit(self.level_image2, (400,15))
 
 def main():
     game = SpaceGame()
